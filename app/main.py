62aaad957839afc4622a73ae68c1dbf4deb6ac47
Marcin <indeed.needit@gmail.com>
2026-07-12 23:40:51 +0200

    feat: add working POST and GET endpoints for drivers

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from app.database import create_db_and_tables
-from app import models
+from app.database import create_db_and_tables, SessionDep
+from app.models import Driver, Car
+from sqlmodel import select
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,3 +14,15 @@ app = FastAPI(title="FleetRent API", lifespan=lifespan)
 @app.get("/")
 def home():
     return {"status": "FleetRent API is running successfully"}
+
+@app.post("/drivers/", response_model=Driver)
+def create_driver(driver: Driver, db: SessionDep):
+    db.add(driver)
+    db.commit()
+    db.refresh(driver)
+    return driver
+
+@app.get("/drivers/", response_model=list[Driver])
+def read_drivers(db: SessionDep):
+    drivers = db.exec(select(Driver)).all()
+    return drivers
